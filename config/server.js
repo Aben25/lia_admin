0584ed0fbfb941e5e8fb298e4a18c7db02674507
Aben25 <abenuro@gmail.com>
2024-09-27 16:10:44 -0400

    liffoy

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
-  url: env('AMPLIFY_URL'),
   host: '0.0.0.0',
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8080),
   app: {
     keys: env.array('APP_KEYS'),
   },
